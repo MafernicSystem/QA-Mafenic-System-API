70768b1a57089148c87e837c6f1a8cb4d3774b9a
BrandonDev <brucecahuanabrandon@gmail.com>
2026-02-15 13:00:17 -0500

    fix: configuracion cors

--- src/config/configCors.ts
+++ src/config/configCors.ts
@@ -1,5 +1,10 @@
 export const corsOptions = {
-  origin: ["http://72.61.53.212", "http://localhost:3000"], // prod + dev
+  origin: [
+    "http://72.61.53.212", 
+    "http://localhost:3000",
+    "http://proyectoqa.online",
+    "http://www.proyectoqa.online"
+  ], // prod + dev
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
